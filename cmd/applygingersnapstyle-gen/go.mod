264b984f2fbebd15702134802750e82d3cb3a875
rigazilla <vrigamon@redhat.com>
2022-12-13 17:54:28 +0100

    added applygingersnapstyle-gen

---
+++ cmd/applygingersnapstyle-gen/go.mod
@@ -0,0 +1,8 @@
+module github.com/gingersnap-api/cmd/applygingersnapstyle-gen
+
+go 1.18
+
+require (
+	github.com/fatih/structtag v1.2.0
+	github.com/iancoleman/strcase v0.2.0
+)
---
+++ cmd/applygingersnapstyle-gen/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
+	"os"
+	"strings"
+
+	"github.com/fatih/structtag"
+	"github.com/iancoleman/strcase"
+)
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+	fset := token.NewFileSet()
+
+	if len(args) < 2 {
+		fmt.Fprintf(os.Stderr, "error: missing arguments, infile outfile needed\n")
+		os.Exit(1)
+	}
+	fileName := args[0]
+	outFile, err := os.Create(args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	// get ast Node of whole file;
+	ff, err := parser.ParseFile(fset, fileName, nil, parser.ParseComments)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	var v visitor
+	ast.Walk(v, ff)
+	if err := format.Node(outFile, fset, ff); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+
+	}
+}
+
+type visitor struct{}
+
+func (v visitor) Visit(n ast.Node) ast.Visitor {
+	if n == nil {
+		return nil
+	}
+	switch field := n.(type) {
+	case *ast.Field:
+		if tag := field.Tag; tag != nil {
+			tags, _ := structtag.Parse(strings.Trim(field.Tag.Value, "`"))
+			for i := range tags.Tags() {
+				if tags.Tags()[i].Key == "json" {
+					tags.Tags()[i].Name = strcase.ToLowerCamel(tags.Tags()[i].Name)
+				}
+				field.Tag.Value = "`" + tags.String() + "`"
+			}
+		}
+	}
+	return v
+}
